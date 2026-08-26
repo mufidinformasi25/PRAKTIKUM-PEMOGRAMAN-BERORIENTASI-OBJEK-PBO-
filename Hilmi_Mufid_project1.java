fd5397b4dcceb220a6b59175e2de6fc4e29c0371
Hilmi Mufid <hilmimufid21@students.untidar.ac.id>
2026-08-26 09:01:27 +0700

    Add files via upload

---
+++ Hilmi_Mufid_project1.java
@@ -0,0 +1,10 @@
+package com.mycompany.hilmi_mufid_project1;
+
+public class Hilmi_Mufid_project1 {
+    public static void main(String[] args) {
+        System.out.println("Nama : Hilmi Mufid");
+        System.out.println("NPM : 2505060046");
+        System.out.println("Alamat : Kabupaten Semarang");
+        System.out.println("No_Hp : 088888888888");
+    }
+}
